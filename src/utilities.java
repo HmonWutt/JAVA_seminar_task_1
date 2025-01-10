361cee65abe96b92db8a0451a31486513a172e87
hmon <wtthumon@gmail.com>
2025-01-10 09:18:23 +0100

    changed message in the input receiver

--- src/utilities.java
+++ src/utilities.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class utilities {
     public static String getChoice(Scanner scanner){
-        System.out.print("Enter your choice: ");
+        System.out.print("Select rock ✊ [r], scissor ✂ [s], paper ✋ [p] or quit [q]: ");
         return scanner.nextLine();
     }
 }
