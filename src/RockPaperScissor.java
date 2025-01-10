ab9e3c9a619983c0724527c1d753be80058045bf
hmon <wtthumon@gmail.com>
2025-01-10 09:40:09 +0100

    made player you static again

--- src/RockPaperScissor.java
+++ src/RockPaperScissor.java
@@ -4,10 +4,10 @@ public class RockPaperScissor {
     static final Scanner scanner = new Scanner(System.in);
     static final Dice dice = new Dice(3);
     static final Player robot = new Player("R2D2");
+    static Player you = new Player("You");
    public static void start(){
        String choice;
        System.out.println("Welcome to a game of rock ✊, scissor ✂, paper ✋! \nYou will play against R2D2 and we keep score.");
-       Player you = new Player("You");
        String myChoice = "-";
        while (!myChoice.equals("q") && !myChoice.equals("Q")){
            robot.playRandomly(dice);
@@ -15,7 +15,7 @@ public class RockPaperScissor {
            try {
                if (myChoice.equals("r") || myChoice.equals("s") || myChoice.equals("p")) {
                    you.playLikeSimpleton(myChoice);
-                   decidesWinner(robot, you);
+                   decidesWinner();
                } else if(myChoice.equals("q")) {
                   break;
                }
@@ -33,20 +33,22 @@ public class RockPaperScissor {
         System.out.println("Select rock ✊ [r], scissor ✂ [s], paper ✋ [p] or quit [q]:");
     }
 
-    private static void decidesWinner(Player robot, Player me) {
+    private static void decidesWinner() {
         String robotStrategy = robot.getStrategy();
-        String myStrategy = me.getStrategy();
+        String myStrategy = you.getStrategy();
         if (myStrategy.equals(robotStrategy)) {
-            System.out.println(String.format("Score: %s, 0 - 0 %s", me.name, robot.name));
-            System.out.println(String.format("Score: %s, 0 - 0 %s", me.name, robot.name));
+            System.out.printf("Score: %s, 0 - 0 %s%n", you.name, robot.name);
+            System.out.printf("Score: %s, 0 - 0 %s%n", you.name, robot.name);
         } else if ((myStrategy.equals("r") && robotStrategy.equals("s")) ||
                 (myStrategy.equals("p") && robotStrategy.equals("r")) ||
                 (myStrategy.equals("s") && robotStrategy.equals("p"))) {
-            System.out.println(String.format("Score: %s, 1 - 0 %s", me.name, robot.name));
+            System.out.printf("Score: %s, 1 - 0 %s%n", you.name, robot.name);
         } else {
-            System.out.println(String.format("Score: %s, 0 - 1 %s", me.name, robot.name));
+            System.out.printf("Score: %s, 0 - 1 %s%n", you.name, robot.name);
         }
     }
+
+
 
 
 }
